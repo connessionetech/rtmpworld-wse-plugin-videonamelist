e63240f56815655f7c9829e9cb8364ba7d07a131
Rajdeep Rath <connessionetechnologies@gmail.com>
2022-12-02 12:36:12 +0530

    Listing update
    + Cleanup
    + Updated Resource class to show more file attributes
    + tested locally

--- src/com/rtmpworld/server/wowza/HTTPMediaListProvider.java
+++ src/com/rtmpworld/server/wowza/HTTPMediaListProvider.java
@@ -3,6 +3,10 @@ package com.rtmpworld.server.wowza;
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -18,155 +22,144 @@ import com.wowza.wms.logging.WMSLoggerFactory;
 import com.wowza.wms.vhost.IVHost;
 
 public class HTTPMediaListProvider extends HTTProvider2Base {
-	
+
 	public static final String WOWZAHOMEDIR = "WMSAPP_HOME";
-	
+
 	private static String PROP_NAME_PREFIX = "medialist";
 	protected static String MODULE_NAME = "ModuleVideoNamesList";
-	
+
 	private static String PROP_DEBUG = PROP_NAME_PREFIX + "Debug";
-	
-	
+
 	private static final Class<HTTPMediaListProvider> CLASS = HTTPMediaListProvider.class;
 	private static final String CLASSNAME = "HTTPMediaListProvider";
-	
-	private static String[] ALLOWED_TYPES = {"mp4", "flv", "f4v", "mp3", "m4a", "3g2", "3gp"};
-	
 
+	private static String[] ALLOWED_TYPES = { "mp4", "flv", "f4v", "mp3", "m4a", "3g2", "3gp" };
 
 	public HTTPMediaListProvider() {
-		
+
 	}
 
-	
 	@Override
 	public void onHTTPRequest(IVHost vhost, IHTTPRequest req, IHTTPResponse res) {
-		
+
 		String queryStr = req.getQueryString();
 		OutputStream out = res.getOutputStream();
 		res.setHeader("Content-Type", "application/json; ; charset=UTF-8");
-		
-		try
-		{
-			
-	        if (queryStr == null)
-	        {
-	            WMSLoggerFactory.getLogger(CLASS).warn(CLASSNAME+".onHTTPRequest: Query string missing");
-	            queryStr = "";
-	        }
-	        
-	        Map<String, String> queryMap = HTTPUtils.splitQueryStr(queryStr);
-	        
-	        
-	        // gather valid requested types
-	        List<String> requested_types = new ArrayList<String>(); 
-	        String[] fileTypes = req.getParameterValues("types");
-	        if(fileTypes != null && fileTypes.length>0) {
-	        	for (String type: fileTypes) {           
-	        	    //Do your stuff here
-	        		if(Arrays.asList(ALLOWED_TYPES).contains(type)){
-	        			requested_types.add(type);
-	        		}
-	        	}
-	        }else {
-	        	requested_types = Arrays.asList(ALLOWED_TYPES);
-	        }
-	        
-	        
-	        // include hls fragments or not
-	        boolean include_hls_fragments = (queryMap.containsKey("include_hls_fragments"))?Boolean.valueOf(queryMap.get("include_hls_fragments")):false;
-	        Resource[] resources = getResources(requested_types, include_hls_fragments);
-	        
-	        SuccessResponse success = new SuccessResponse();
-	        success.data = resources;
-	        
-	        
+
+		try {
+
+			if (queryStr == null) {
+				WMSLoggerFactory.getLogger(CLASS).warn(CLASSNAME + ".onHTTPRequest: Query string missing");
+				queryStr = "";
+			}
+
+			Map<String, String> queryMap = HTTPUtils.splitQueryStr(queryStr);
+
+			// gather valid requested types
+			List<String> requested_types = new ArrayList<String>();
+			String[] fileTypes = req.getParameterValues("types");
+			if (fileTypes != null && fileTypes.length > 0) {
+				for (String type : fileTypes) {
+					// Do your stuff here
+					if (Arrays.asList(ALLOWED_TYPES).contains(type)) {
+						requested_types.add(type);
+					}
+				}
+			} else {
+				requested_types = Arrays.asList(ALLOWED_TYPES);
+			}
+
+			// include hls fragments or not
+			boolean include_hls_fragments = (queryMap.containsKey("include_hls_fragments"))
+					? Boolean.valueOf(queryMap.get("include_hls_fragments"))
+					: false;
+			Resource[] resources = getResources(requested_types, include_hls_fragments);
+
+			SuccessResponse success = new SuccessResponse();
+			success.data = resources;
+
 			byte[] outBytes = new Gson().toJson(success).toString().getBytes();
 			out.write(outBytes);
 
-		}
-		catch(Exception e)
-		{
+		} catch (Exception e) {
 			ErrorResponse error = new ErrorResponse();
 			error.code = 400;
 			error.data = e.getMessage();
-		        
-		        
+
 			byte[] outBytes = new Gson().toJson(error).toString().getBytes();
-			
-			try 
-			{
+
+			try {
 				out.write(outBytes);
-			} 
-			catch (IOException e1) 
-			{
-				WMSLoggerFactory.getLogger(CLASS).error(CLASSNAME+".onHTTPRequest: " + e1.getMessage());
+			} catch (IOException e1) {
+				WMSLoggerFactory.getLogger(CLASS).error(CLASSNAME + ".onHTTPRequest: " + e1.getMessage());
 			}
 		}
 	}
-	
-	
-	class SuccessResponse extends Response{
+
+	class SuccessResponse extends Response {
 		String type = "data";
-		
+
 		public SuccessResponse() {
 			this.code = 200;
 		}
 	}
-	
-	
-	class ErrorResponse extends Response{		
+
+	class ErrorResponse extends Response {
 		String type = "error";
 	}
-	
-	
-	class Response{		
+
+	class Response {
 		int code;
 		Object data;
+
 		public int getCode() {
 			return code;
 		}
+
 		public void setCode(int code) {
 			this.code = code;
 		}
+
 		public Object getData() {
 			return data;
 		}
+
 		public void setData(Object data) {
 			this.data = data;
 		}
 	}
-	
-	
-	
-	
+
 	class Resource {
-		File f;
+		String name;
+		String created;
+		String modified;
+		long size;
 
-		public Resource(File f) {
-			this.f = f;
-		}
+		public Resource(File f) throws IOException {
 
-		public File getFile() {
-			return f;
+			Path file = Paths.get(f.getAbsolutePath());
+			BasicFileAttributes attr = Files.readAttributes(file, BasicFileAttributes.class);
 
+			name = f.getName();
+			size = Files.size(file);
+			created = attr.creationTime().toString();
+			modified = attr.lastModifiedTime().toString();
 		}
 	}
 	
 	
 	
-	public Resource[] getResources(List<String> types, boolean include_hls_fragments) 
-	{
+	
+
+	public Resource[] getResources(List<String> types, boolean include_hls_fragments) {
 		String wowzaHomeDir = System.getenv(WOWZAHOMEDIR);
 		String storageDir = "content";
 
 		if (wowzaHomeDir == null) {
-			throw new RuntimeException("Environmnet variable WMSAPP_HOME is empty");
-		}
-		else if (!wowzaHomeDir.endsWith("/") && !wowzaHomeDir.endsWith("\\")) {
+			throw new RuntimeException("Environment variable WMSAPP_HOME is empty");
+		} else if (!wowzaHomeDir.endsWith("/") && !wowzaHomeDir.endsWith("\\")) {
 			storageDir = wowzaHomeDir + File.separatorChar + storageDir;
-		}
-		else {
+		} else {
 			storageDir = wowzaHomeDir + storageDir;
 		}
 		File fStorageDir = new File(storageDir);
@@ -176,24 +169,24 @@ public class HTTPMediaListProvider extends HTTProvider2Base {
 		for (File file : fStorageDir.listFiles()) {
 			if (file.isFile()) {
 				String extension = getExtensionByStringHandling(file.getName()).get();
-				if(types.contains(extension)) {
-					res.add(new Resource(file));
+				if (types.contains(extension)) {
+					try {
+						res.add(new Resource(file));
+					} catch (IOException e) {
+						WMSLoggerFactory.getLogger(CLASS).error(CLASSNAME + ".getResources: Unable to gather info for file " + file.getAbsolutePath());
+					}
 				}
 			}
 		}
-		
+
 		Resource[] resources = res.toArray(new Resource[res.size()]);
 		return resources;
 
 	}
-	
-	
-	
-	
+
 	public Optional<String> getExtensionByStringHandling(String filename) {
-	    return Optional.ofNullable(filename)
-	      .filter(f -> f.contains("."))
-	      .map(f -> f.substring(filename.lastIndexOf(".") + 1));
+		return Optional.ofNullable(filename).filter(f -> f.contains("."))
+				.map(f -> f.substring(filename.lastIndexOf(".") + 1));
 	}
 
 }
--- src/com/rtmpworld/server/wowza/ModuleVideoNamesList.java
+++ src/com/rtmpworld/server/wowza/ModuleVideoNamesList.java
@@ -1,15 +1,7 @@
 package com.rtmpworld.server.wowza;
 
 import com.wowza.wms.application.*;
-import com.wowza.wms.amf.*;
-import com.wowza.wms.client.*;
 import com.wowza.wms.module.*;
-import com.wowza.wms.request.*;
-import com.wowza.wms.stream.*;
-import com.wowza.wms.rtp.model.*;
-import com.wowza.wms.httpstreamer.model.*;
-import com.wowza.wms.httpstreamer.cupertinostreaming.httpstreamer.*;
-import com.wowza.wms.httpstreamer.smoothstreaming.httpstreamer.*;
 import com.wowza.wms.logging.WMSLogger;
 
 public class ModuleVideoNamesList extends ModuleBase {
